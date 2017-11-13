2d20e7e594cfaf1c2abd334495faa736da03ecff
willschwarzer <31933980+willschwarzer@users.noreply.github.com>
2017-11-13 17:49:14 -0600

    deleted extraneous import

--- src/game/GameFrame.java
+++ src/game/GameFrame.java
@@ -6,7 +6,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 /**
  * An implementation of Java's JFrame with a purpose of displaying a window we can put elements in.
