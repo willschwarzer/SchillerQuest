00fdcda41e900ca126d5a1ef020c760dcebb06d3
Ephraim Benson <ephraimbenson@users.noreply.github.com>
2017-11-13 20:38:03 -0600

    dynamically adding inventory items

--- src/game/InventoryPane.java
+++ src/game/InventoryPane.java
@@ -6,45 +6,94 @@ import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyleContext;
 import java.awt.*;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.ArrayList;
 
 public class InventoryPane extends JTextPane {
+	private ArrayList<String> backpack = new ArrayList<>();
+	private ArrayList<String> equipped = new ArrayList<>();
+
 	public InventoryPane() {
     	super();
-		drawPane();
 
-		setBackground(Color.black);
-		setFont(new Font(Font.MONOSPACED, Font.PLAIN, 18));
-		setEditable(false);
+		//**
+		backpack.add("Cool Sword");
+		backpack.add("Rusty Spoon");
+		backpack.add("Freedom Key");
+		equipped.add("Cooler Sword");
+		equipped.add("Modest Loins");
+		equipped.add("Justice Amulet");
+		//**
+
+		drawPane();
 	}
 
 	private void drawPane() {
 		setEditable(true);
 		setText("");
-
-		// temp implementation of loading dummy inventory from file
-		File levelFile = new File("src/resources/inventory.txt");
-
-		try (BufferedReader br = new BufferedReader(new FileReader(levelFile))) {
-			String line;
-			while ((line = br.readLine()) != null) {
-				for (char c : line.toCharArray()) {
-					appendText(Character.toString(c), colorForChar(c));
-				}
-				appendText("\n", Color.white);
-			}
-		} catch (IOException e) {
-			System.out.print(e.getMessage());
-		}
-		// end temp implementation
-
+		addHeaderText();
+		buildInventoryList();
 		setEditable(false);
 		setBackground(Color.black);
 		setFont(new Font(Font.MONOSPACED, Font.PLAIN, 18));
 		setEditable(false);
+	}
+
+	private void addHeaderText() {
+		String header =
+				"                            _      ___  _   _                  \n" +
+				"               | |\\ | \\  / |_ |\\ |  |  | | |_| \\ /             \n" +
+				"               | | \\|  \\/  |_ | \\|  |  |_| | \\  |              \n" +
+				"                                                               ";
+		addLine(header);
+	}
+
+	private void addItemPair(String backpackItem, String equippedItem) {
+		char[] bpChars = backpackItem.toCharArray();
+		char[] eqChars = equippedItem.toCharArray();
+
+		int n = 0;
+		while (n < 16) {
+			appendText(" ", Color.white);
+			n++;
+		}
+		int i = 0;
+		while (i < bpChars.length) {
+			appendText(Character.toString(bpChars[i]), colorForChar(bpChars[i]));
+			i++;
+		}
+		int index = i + n;
+		while (index < 38) {
+			appendText(" ", Color.white);
+			index++;
+		}
+		for (char c : eqChars) {
+			appendText(Character.toString(c), colorForChar(c));
+		}
+		appendText("\n", Color.white);
+	}
+
+	private void addLine(String line) {
+		for (char c : line.toCharArray()) {
+			appendText(Character.toString(c), colorForChar(c));
+		}
+		appendText("\n", Color.white);
+	}
+
+	private void buildInventoryList() {
+		addLine("                 Backpack:             Equipped:                  ");
+		addLine("                 =========             =========                  ");
+
+		int i = 0;
+		while (i < backpack.size() || i < equipped.size()) {
+			if (i < backpack.size() && i < equipped.size()) {
+				addItemPair(backpack.get(i), equipped.get(i));
+			} else if (i < backpack.size() && i >= equipped.size()) {
+				addItemPair(backpack.get(i), "");
+			} else if (i >= backpack.size() && i < equipped.size()) {
+				addItemPair("", equipped.get(i));
+			}
+			i++;
+		}
 	}
 
 	private void appendText(String text, Color c) {
